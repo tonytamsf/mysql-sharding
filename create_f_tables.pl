061c9e9fe31566eb5de0eb7e1b6db2661bdfa591
Tony Tam <tonytam@yahoo.com>
2013-05-11 23:03:53 -0700

    initial version

---
+++ create_f_tables.pl
@@ -0,0 +1,23 @@
+#!/home/y/bin/perl -w
+
+#drop table user_history_XX;
+my $S_string = qq|
+CREATE TABLE user_history_XX (
+  itemid int(11) unsigned NOT NULL default '0',
+  uid int(11) unsigned NOT NULL default '0',
+  lastupdated timestamp(14) NOT NULL,
+  PRIMARY KEY  (itemid,uid),
+  KEY lastupdated (lastupdated),
+  KEY uid (uid)
+) TYPE=INNODB;
+|;
+
+
+my $S_i = 0;
+while ($S_i <= 0xf) {
+  my $S_hex = sprintf("%02x", $S_i);
+  $S_i++;
+  $S_sql = $S_string;
+  $S_sql =~ s/XX/$S_hex/g;
+  print $S_sql,"\n";
+}
